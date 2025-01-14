e4c2fb730c507a86c400f6bca5f76581694472d6
ozancck <ozancck@icloud.com>
2025-01-14 03:49:18 +0300

    Fix file paths in index.html

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/ozancck.github.io/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
 })
